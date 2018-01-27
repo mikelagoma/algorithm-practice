bd2eb684efdbfec401f3ab193a1054265633342f
mikelagoma <mike.lagoma@gmail.com>
2018-01-27 00:14:16 -0800

    Tree: Postorder Traversal

--- hacker-rank_tree_problems.py
+++ hacker-rank_tree_problems.py
@@ -11,3 +11,18 @@ def preOrder(root):
         preOrder(root.left)
     if root.right:
         preOrder(root.right)
+
+# https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def postOrder(root):
+    if root.left:
+        postOrder(root.left)
+    if root.right:
+        postOrder(root.right)
+    print root.data,
+
