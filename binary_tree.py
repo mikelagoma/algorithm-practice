458ad09d5430c946ab433c645b38d07f914132ce
mikelagoma <mike.lagoma@gmail.com>
2018-01-26 23:47:53 -0800

    Initial binary tree class commit

---
+++ binary_tree.py
@@ -0,0 +1,20 @@
+
+class TreeNode():
+    def __init__(self):
+        self.value = None
+        self.left = None
+        self.right = None
+
+    
+
+class Tree():
+    def __init__(self):
+        self.root = None
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
