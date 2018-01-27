3ab810a6db8aa3a40308efe3ab4f70f0c6412457
mikelagoma <mike.lagoma@gmail.com>
2018-01-27 01:03:39 -0800

    Tree: Height of a Binary Tree

--- hacker-rank_tree_problems.py
+++ hacker-rank_tree_problems.py
@@ -41,3 +41,31 @@ def inOrder(root):
     if root.right:
         inOrder(root.right)
 
+#https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+mheight = 0
+current_height = 0
+def height(root):
+    global mheight
+    global current_height
+    if not root.left and not root.right:
+        if current_height > mheight:
+            mheight = current_height
+            current_height = current_height - 1
+    else:
+        current_height += 1
+    if root.left:
+        height(root.left)
+    if root.right:
+        height(root.right)
+    return mheight
+    
