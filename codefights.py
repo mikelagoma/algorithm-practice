5e1849197d9586190d445df05bfc32f3747b600b
mikelagoma <mike.lagoma@gmail.com>
2018-02-25 21:35:26 -0800

    48 - Cyclic Name

--- codefights.py
+++ codefights.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from collections import Counter
-
+from itertools import cycle
 
 #https://codefights.com/arcade/python-arcade/caravan-of-collections/pE4t3DcoTRfwHwYG8 
 def frequencyAnalysis(encryptedText):
@@ -11,11 +11,16 @@ def frequencyAnalysis(encryptedText):
     # print(c['C'])
     return c.most_common(1)[0][0]
 
+def cyclicName(name, n):
+    gen = cycle(name)
+    res = [next(gen) for _ in range(n)]
+    return ''.join(res)
 
 
 def main():
-    encryptedText = "$~NmiNmim$/NVeirp@dlzrCCCCfFfQQQ"
-    print(frequencyAnalysis(encryptedText))
+    # encryptedText = "$~NmiNmim$/NVeirp@dlzrCCCCfFfQQQ"
+    # print(frequencyAnalysis(encryptedText))
+    print(cyclicName('nicecoder', 15))
     return
 
 if __name__ == '__main__':
